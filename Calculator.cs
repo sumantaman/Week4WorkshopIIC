0dcab5c05d9074febe60f5f659e5c1bceba5a6d4
sumantaman <161899459+sumantaman@users.noreply.github.com>
2025-11-19 08:27:44 +0545

    first commit

---
+++ Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop_Week_4
+{
+    public class Calculator
+    {
+        public string PrintWelcome()
+        {
+            return "Welcome to the Calculator";
+        }
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Multiply(int x, int y) 
+            {
+                return x * y;
+            }
+    }
+}
